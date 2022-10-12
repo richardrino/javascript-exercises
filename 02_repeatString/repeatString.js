1156055789810c47ad028b1601b1cc42cb8f0fe0
Richard Rino <richardvrino@gmail.com>
2022-10-12 17:51:19 -0500

    Complete JavaScript exercises
    Completed the following exercises:
      - 01_helloWorld
    	- 02_repeatString
    	- 03_reverseString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function (inputString, loopNum) {
+  if (loopNum < 0) {
+    return "ERROR";
+  }
 
+  let repeatString = "";
+
+  for (let i = 0; i < loopNum; i++) {
+    repeatString += inputString;
+  }
+
+  return repeatString;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function (stringToReverse) {
+  let stringToReverseArray = Array.from(stringToReverse);
+  let reverseString = "";
 
+  for (let i = stringToReverse.length; i > 0; i--) {
+    reverseString += stringToReverseArray.pop();
+  }
+
+  return reverseString;
 };
 
 // Do not edit below this line
